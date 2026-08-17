0bbb012205a28023bd4a054f3c2702ed82a00a43
ribeiroyara8 <ribeiroyara894@gmail.com>
2026-08-17 14:48:12 -0300

    revisão e acumulo de variaveis

---
+++ 03_revisao.js
@@ -0,0 +1,35 @@
+
+let aluguel = 1200.00
+let condominio = 250.59
+let internet = 85.99
+let total = aluguel + condominio + internet
+
+console.log("O total das contas é: " +total)
+
+
+// -------------------------------------------
+
+let nome = "Yara"
+let sobrenome = "Ribeiro"
+let nome_completo = nome +" "+ sobrenome
+
+console.log("Seja bem-vindo(a) " + nome_completo)
+
+// -------------------------------------------
+
+let salario = 2000
+console.log("Seu salário é: R$ "+salario)
+
+let comissao = 100
+salario = salario + comissao //acumolo: adicionar um novo valor na variavel
+console.log("Salário com comissão: R$"+salario)
+
+let adicional_noturno = 200
+salario = salario + comissao + adicional_noturno
+console.log("Salário com adicional noturno: R$" +salario)
+
+/*
+Isso é um comentario
+de multiplas linhas
+a vontade
+*/
